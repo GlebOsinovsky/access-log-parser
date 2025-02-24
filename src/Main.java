1ad9c64652f6b7d4f6fb16a0294c5e6b004c87bd
GlebOsinovsky <oga.nvr@yandex.ru>
2025-02-24 21:49:17 +0300

    Добавлены операции с числами в новую ветку 2-number-expirements

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String scanner = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + scanner.length());
+        System.out.println("Введите число и нажмите <Enter>: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите <Enter>: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Первое число: " + firstNumber + "\nВторое число: " + secondNumber);
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма: " + sum);
+        int diff = firstNumber - secondNumber;
+        System.out.println("Разность: " + diff);
+        int product = firstNumber * secondNumber;
+        System.out.println("Произведение: " + product);
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
     }
 }
