eb30f1380361fe9eac3c92388709103daaadb090
GlebOsinovsky <oga.nvr@yandex.ru>
2025-03-08 22:34:28 +0300

    В master добавлен код для Курсового проекта.Задание по теме "Циклы"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.io.File;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String scanner = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + scanner.length());
+        Scanner scanner = new Scanner(System.in);
+        String filePath;
+        int rightPathamount=0;
+        while (true) {
+            System.out.println("Введите полный путь к файлу:");
+            filePath = scanner.nextLine();
+
+            File file = new File(filePath);
+
+            boolean fileExists = file.isFile();
+            boolean isDirectory = file.isDirectory();
+
+            if (fileExists == true) {
+                rightPathamount++;
+                System.out.println("Путь к файлу указан верно");
+                System.out.println("Это файл номер "+rightPathamount);
+                ;
+            } else if ( isDirectory == true) {
+                System.out.println("Указанный путь является путём к папке");continue;}
+                            else {
+                    System.out.println("Указаный файл не существует, либо путь указан неверно");continue;
+                }
+            }
+        }
     }
-}
+
+
