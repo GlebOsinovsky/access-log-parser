f52258946ffad66a554c04f967199adf7e8c6a06
GlebOsinovsky <oga.nvr@yandex.ru>
2025-04-05 11:53:33 +0300

    Решение Полиморфизм #2. Замкнутая ломаная

---
+++ src/ClosedPolyLine.java
@@ -0,0 +1,25 @@
+import javafx.scene.shape.LineTo;
+
+public class ClosedPolyLine extends PolyLine {
+
+    public ClosedPolyLine() {
+        super();
+    }
+
+    public ClosedPolyLine(Point... initialPoints) {
+        super(initialPoints);
+    }
+    @Override
+    public double getLength (){
+        double originalLength = super.getLength();
+        if (points.size() < 2) return originalLength;
+        Line closingLine = new Line(points.get(points.size() - 1), points.get(0));
+
+        return originalLength + closingLine.getLength();
+    }
+
+    @Override
+    public String toString() {
+        return "Замкнутая " + super.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -14,29 +14,14 @@ public class Main {
         Point d = new Point(8,9);
 
 
-        System.out.println("До изменения точки {2,8}");
         PolyLine polyLine = new PolyLine(a,b,c,d);
         System.out.println(polyLine);
         System.out.println("Длина ломаной: "+polyLine.getLength());
-        Line[] lines = polyLine.getLines();
-        System.out.println("Массив линий: "+Arrays.toString(lines));
-        System.out.println("Длина массива линий: "+ lines.length);
-        double linesLength = 0.0;
-        for (Line line : lines) {
-            linesLength += line.getLength();
-        }
-        System.out.println("Сумма длин линий: "+ linesLength);
-        double polyLength = polyLine.getLength();
-        System.out.println("Длины совпадают: " + (polyLength == linesLength));
+        Line line = new Line(d,a);
+        System.out.println("Длина замыкающей линии "+line.getLength());
 
-        System.out.println(" ");
-        System.out.println("После изменения точки{2,8} на {12,8}");
-        b.x = 12;
-        System.out.println("Изменения в точке"+b);
-        System.out.println(polyLine);
-        System.out.println("Изменения в ломаной: "+polyLine);
-        System.out.println("Изменения в 2х линиях массива: "+Arrays.toString(lines));
-
+        ClosedPolyLine closedPolyLine = new ClosedPolyLine(a,b,c,d);
+        System.out.println("Длина замкнутой: "+closedPolyLine.getLength());
 
     }
 }
