2865000208e70d535500a5ef1d70680c7ecc7091
GlebOsinovsky <oga.nvr@yandex.ru>
2025-04-24 00:38:05 +0300

    Решение Задание #1. Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.sql.SQLOutput;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String filePath;
@@ -20,12 +22,44 @@ public class Main {
                 rightPathamount++;
                 System.out.println("Путь к файлу указан верно");
                 System.out.println("Это файл номер "+rightPathamount);
-                ;
+
+                try (FileReader fileReader = new FileReader(filePath);
+                    BufferedReader reader = new BufferedReader(fileReader))
+                {
+                int linesCount = 0;
+                int maxLength = 0;
+                int minLength = Integer.MAX_VALUE;
+                String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+
+                        if (line.length() > 1024){
+                            throw new LongLineException("Ошибка! Строка"+ (linesCount + 1) +" содержит "+ line.length() +" символов.");
+                        }
+                        linesCount++;
+                        maxLength = Math.max(maxLength, line.length());
+                        minLength = Math.min(minLength, line.length());
+                    }
+                    if (linesCount ==0) minLength =0;
+
+                    System.out.println("Общее кол-во строк "+linesCount);
+                    System.out.println("Длина самой длинной строки "+maxLength);
+                    System.out.println("Длина самой короткой строки" + minLength);
+
+                } catch (LongLineException e){System.err.println(e.getMessage());
+                } catch (Exception ex) {
+                    System.err.println("Ошибка при чтении файла: " + ex.getMessage());
+                }
+
+
             } else if ( isDirectory == true) {
-                System.out.println("Указанный путь является путём к папке");continue;}
-                            else {
+                System.out.println("Указанный путь является путём к папке");continue;
+            }
+                else {
                     System.out.println("Указаный файл не существует, либо путь указан неверно");continue;
                 }
+
             }
         }
     }
