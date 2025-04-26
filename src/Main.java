58bb24d2d7918500c3a138772bd48c5291a189b8
GlebOsinovsky <oga.nvr@yandex.ru>
2025-04-26 19:19:30 +0300

    Решение Задание #2. Сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String filePath;
@@ -26,9 +27,10 @@ public class Main {
                 try (FileReader fileReader = new FileReader(filePath);
                     BufferedReader reader = new BufferedReader(fileReader))
                 {
-                int linesCount = 0;
-                int maxLength = 0;
-                int minLength = Integer.MAX_VALUE;
+                    int linesCount = 0;
+                    int googlebotCount = 0;
+                    int yandexbotCount = 0;
+
                 String line;
 
                     while ((line = reader.readLine()) != null) {
@@ -38,14 +40,73 @@ public class Main {
                             throw new LongLineException("Ошибка! Строка"+ (linesCount + 1) +" содержит "+ line.length() +" символов.");
                         }
                         linesCount++;
-                        maxLength = Math.max(maxLength, line.length());
-                        minLength = Math.min(minLength, line.length());
-                    }
-                    if (linesCount ==0) minLength =0;
+// далее для каждой строки извленкаем компоненты
 
+                        // извлечение IP-адреса
+                        int firstSpace = line.indexOf(' ');
+                        String ip = line.substring(0, firstSpace);
+
+                        // извлечение двух пропущенных свойств
+                        String rest = line.substring(firstSpace + 1);
+                        int nextSpace1 = rest.indexOf(' ');
+                        int nextSpace2 = rest.indexOf(' ', nextSpace1 + 1);
+                        String prop1 = rest.substring(0, nextSpace1);
+                        String prop2 = rest.substring(nextSpace1 + 1, nextSpace2);
+                        rest = rest.substring(nextSpace2 + 1);
+
+                        // извлечение даты и времени
+                        int bracketStart = rest.indexOf('[');
+                        int bracketEnd = rest.indexOf(']');
+                        String dateTime = rest.substring(bracketStart + 1, bracketEnd);
+                        rest = rest.substring(bracketEnd + 2);
+
+                        // извлечение метода и пути
+                        int quoteStart = rest.indexOf('"');
+                        int quoteEnd = rest.indexOf('"', quoteStart + 1);
+                        String request = rest.substring(quoteStart + 1, quoteEnd);
+                        String[] requestParts = request.split(" ");
+                        String method = requestParts.length > 0 ? requestParts[0] : "";
+                        String path = requestParts.length > 1 ? requestParts[1] : "";
+                        rest = rest.substring(quoteEnd + 2);
+
+                        // извлечение HTTP-кода и размера данных
+                        int space1 = rest.indexOf(' ');
+                        int space2 = rest.indexOf(' ', space1 + 1);
+                        String httpCode = rest.substring(0, space1);
+                        String dataSize = rest.substring(space1 + 1, space2);
+                        rest = rest.substring(space2 + 1);
+
+                        // извлечение referer
+                        quoteStart = rest.indexOf('"');
+                        quoteEnd = rest.indexOf('"', quoteStart + 1);
+                        String referer = rest.substring(quoteStart + 1, quoteEnd);
+                        rest = rest.substring(quoteEnd + 2);
+
+                        // извлечение User-Agent
+                        quoteStart = rest.indexOf('"');
+                        quoteEnd = rest.indexOf('"', quoteStart + 1);
+                        String userAgent = rest.substring(quoteStart + 1, quoteEnd);
+
+                        System.out.println("User-Agent: " + userAgent);
+                        System.out.println(parseUserAgent(userAgent));
+                        parseUserAgent(userAgent);
+
+                        String bot = parseUserAgent(userAgent);
+                        if ("Googlebot".equals(bot)) {
+                            googlebotCount++;
+                        } else if ("YandexBot".equals(bot)) {
+                            yandexbotCount++;
+                        }
+                    }
+                    double googlebotPart = (100.0*googlebotCount)/linesCount;
+                    double yandexbotPart = (100.0*yandexbotCount)/linesCount;
+
+                    System.out.println("кол-во Googlebot " +googlebotCount);
+                    System.out.println("кол-во YandexBot " +yandexbotCount);
+                    System.out.println("кол-во запросов " +linesCount);
                     System.out.println("Общее кол-во строк "+linesCount);
-                    System.out.println("Длина самой длинной строки "+maxLength);
-                    System.out.println("Длина самой короткой строки" + minLength);
+                    System.out.println("доля запросов от YandexBot: " + yandexbotPart);
+                    System.out.println("доля запросов от Googlebot: " +googlebotPart);
 
                 } catch (LongLineException e){System.err.println(e.getMessage());
                 } catch (Exception ex) {
@@ -59,9 +120,27 @@ public class Main {
                 else {
                     System.out.println("Указаный файл не существует, либо путь указан неверно");continue;
                 }
-
             }
         }
+
+    private static String parseUserAgent(String userAgent) {
+        try {
+            int start = userAgent.indexOf('(');
+            int end = userAgent.indexOf(')', start + 1);
+            if (start == -1 || end == -1) return null;
+
+            String bracketContent = userAgent.substring(start + 1, end);
+            String[] parts = bracketContent.split(";");
+            if (parts.length >= 2) {
+                String fragment = parts[1];
+            }
+            String fragment = parts[1].trim();
+            String userAgentPart = fragment.split("/")[0].trim();
+            return userAgentPart;
+        } catch (Exception e) {
+            return null;
+        }
+    }
     }
 
 
